a32a31346683e0b315f1e4b68452ae64d166b2fd
rasitayaz <mrasitayaz@gmail.com>
2022-12-01 12:13:03 +0300

    update example

--- example/hasura_helper_example.dart
+++ example/hasura_helper_example.dart
@@ -1,13 +1,77 @@
 import 'package:hasura_helper/hasura_helper.dart';
 
 void main() {
-  final request = Hasura.insert(
+  Hasura? request;
+
+  /// basic usage
+
+  request = Hasura.insert(
     table: 'users',
     object: {
       'username': 'rasitayaz',
       'profession': 'developer',
     },
     returning: {'id'},
+  );
+
+  print(request.body);
+
+  /// multiple queries (or mutations)
+
+  request = Hasura.query(
+    actions: [
+      HasuraQuery.list(
+        table: 'users',
+        where: {
+          'profession': {'_eq': 'developer'},
+        },
+        returning: {'id', 'username'},
+      ),
+      HasuraQuery.byId(
+        table: 'users',
+        id: 1,
+        returning: {'id', 'profession'},
+      ),
+    ],
+  );
+
+  print(request.body);
+
+  /// with jsonb variables
+
+  request = Hasura.updateById(
+    table: 'users',
+    id: 1,
+    variables: {
+      'education': {
+        'school': 'marmara university',
+        'department': 'computer engineering',
+      },
+    },
+    args: {
+      '_set': {'education': '\$education'},
+    },
+    returning: {'id'},
+  );
+
+  print(request.body);
+
+  /// using entities for relations
+
+  request = Hasura.queryById(
+    table: 'users',
+    id: 1,
+    returning: {
+      'id',
+      HasuraEntity(
+        'messages',
+        args: {
+          'limit': 20,
+          'order_by': {'date': 'desc'},
+        },
+        fields: {'id', 'text'},
+      ),
+    },
   );
 
   print(request.body);
